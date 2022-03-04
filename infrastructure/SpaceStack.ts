f5103f483e0b2cae87a120d86c7cfc3c58439ad5
최낙원님/디지털채널서비스팀 <cnw529@naver.com>
2022-03-04 11:49:41 +0900

    Hello API & Lambda Integration

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -2,10 +2,13 @@ import { join } from 'path';
 
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 
 export class SpaceStack extends Stack {
+
+  private api = new RestApi(this, 'SpaceApi');
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
@@ -14,6 +17,11 @@ export class SpaceStack extends Stack {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main'
-    }); 
+    });
+
+    // Hello API lambda integration:(
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello');
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
 }
