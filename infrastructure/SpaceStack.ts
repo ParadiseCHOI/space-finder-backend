7ba53b06eb74322bbd3c003b79de809e8bbef9c7
최낙원님/디지털채널서비스팀 <cnw529@naver.com>
2022-03-23 13:33:17 +0900

    Add an example code using aws-sdk, S3

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -7,6 +7,7 @@ import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { GenericTable } from './GenericTable';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { handler } from '../services/node-lambda/hello';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 
 export class SpaceStack extends Stack {
@@ -21,19 +22,24 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new Function(this, 'helloLambda', {
-      runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-      handler: 'hello.main'
-    });
+    // const helloLambda = new Function(this, 'helloLambda', {
+    //   runtime: Runtime.NODEJS_14_X,
+    //   code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+    //   handler: 'hello.main'
+    // });
 
     const helloLambdaNodejs = new NodejsFunction(this, 'helloLambdaNodejs', {
       entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
       handler: 'handler'
     });
-
+    
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions("s3:ListAllMyBuckets");
+    s3ListPolicy.addResources('*');
+    helloLambdaNodejs.addToRolePolicy(s3ListPolicy);
+    
     // Hello API lambda integration:(
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodejs);
     const helloLambdaResource = this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
